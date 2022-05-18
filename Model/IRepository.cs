7dce443acd29cad1fc8163ff9657b75a4b8025c3
João Xavier <fastjonh@gmail.com>
2022-05-18 18:15:22 -0300

    Add project files.

---
+++ Model/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Model
+{
+    public interface IRepository<T>
+    {
+        public T GetById(Guid id);
+        public List<T> GetRange(int range = 20);
+        public void Insert(T entity);
+        public void Update(Guid id, T data);
+        public void Delete(Guid id);
+    }
+}

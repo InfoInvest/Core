749bf499a2c5ee78f5107e680a79fa154a180c6f
João Xavier <fastjonh@gmail.com>
2022-05-23 12:28:39 -0300

    nullable get by id

--- Core.Model/IRepository.cs
+++ Core.Model/IRepository.cs
@@ -2,7 +2,7 @@
 {
     public interface IRepository<T>
     {
-        public T GetById(Guid id);
+        public T? GetById(Guid id);
         public List<T> GetRange(int range = 20);
         public void Add(T entity);
     }
